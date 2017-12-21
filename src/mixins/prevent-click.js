4bb62b6ad3b2e6bfe0cc30bf64994529f50548ad
hdq <haqsir@163.com>
2017-12-21 19:04:01 +0800

    添加选择年级

--- src/mixins/prevent-click.js
+++ src/mixins/prevent-click.js
@@ -7,8 +7,8 @@ module.exports = {
   methods: {
     preventClick () { // 防止按钮重复提交
       let nowTime = new Date().getTime()
-      let clickBeforeTime = this.clickTime
-      if (this.clickTime !== null && nowTime - clickBeforeTime < 1000) {
+      let clickAfterTime = this.clickTime
+      if (this.clickTime !== null && nowTime - clickAfterTime < 1000) {
         console.log('防止重复提交')
         return false
       } else {
--- src/page/common/main.vue
+++ src/page/common/main.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="main">
-    <h2>学前班 第一单元 <img src="static/img/btn_Modify.png" alt=""></h2>
+    <h2>学前班 一单元 <img src="static/img/btn_Modify.png" alt="" @click="showSelGrade"></h2>
+   <div class="show-select" v-if="showSel">
+     <div class="wrop"></div>
+     <div class="sele">
+        <span class="sel-init sel-grade">学前班</span>
+        <span class="sel-init sel-unit">一单元</span>
+       <img src="static/img/sel_grade.png" alt="">
+       <span class="start" @click="showSelGrade">开始</span>
+       <!-- <label>年级</label>
+       <select name="grade" >
+             <option value="学前班">学前班</option>
+       </select>
+       <label>单元</label>
+       <select name="unit" >
+         <option value="一单元">一单元</option>
+       </select> -->
+     </div>
+   </div>
     <div class="wechat">
       <img src="static/img/main_we.png" alt="" width="95%"height="100%">
     </div>
@@ -37,13 +54,17 @@ import { mapActions, mapGetters } from 'vuex'
 export default {
   data () {
     return {
+      showSel: true
     }
   },
   computed: {
     ...mapGetters('common/wordsStore', ['getAllWords'])
   },
   methods: {
-    ...mapActions('getWords', ['words'])
+    ...mapActions('getWords', ['words']),
+    showSelGrade () {
+     this.showSel = !this.showSel
+    }
   }
 }
 </script>
