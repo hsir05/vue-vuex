7356f9862c2c665cfe48d06d2eec0c9ed5857c7f
hdq <haqsir@163.com>
2017-12-27 20:06:41 +0800

    autoplay test

--- src/page/learn_word/next-btn.vue
+++ src/page/learn_word/next-btn.vue
@@ -39,12 +39,14 @@ export default {
     }
   },
   mounted () {
-    this.soundOpen()
+    // this.soundOpen()
+    this.autoPlayAudio()
     this.audioSecond = this.getIndexWord.type[0]
   },
   watch: {
     flag () {
-      this.soundOpen()
+      // this.soundOpen()
+      this.autoPlayAudio()
     }
   },
   computed: {
