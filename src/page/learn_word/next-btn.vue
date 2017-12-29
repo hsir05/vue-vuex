8fcc64f698407d41046df8634cd6c7e173c6a6f6
hdq <haqsir@163.com>
2017-12-29 12:08:31 +0800

    正确音频与错误音频处理

--- src/page/learn_word/next-btn.vue
+++ src/page/learn_word/next-btn.vue
@@ -4,9 +4,9 @@
           <img src="static/img/btn_next.png" v-if="flag !== 3">
           <i v-if="flag === 3" :class="seIndex===null?'chanColor':''">确定</i>
           <!-- 正确音效 -->
-          <audio src="static/audio/right.wav" ref="right" > </audio>
+          <audio src="static/audio/right.mp3"  ref="right" > </audio>
            <!-- 错误音效 -->
-          <audio src="static/audio/error.wav" ref="error" > </audio>
+          <audio src="static/audio/error.mp3"  ref="error" > </audio>
         </span>
         <span class="sound" v-if="flag !== 3" @click="soundOpen(flag)">
           <img src="static/img/btn_sound.png" alt="" >
@@ -94,8 +94,8 @@ export default {
               this.three()
           }, 2000)
           } else { // 选择错误
-            this.$store.commit('learnWords/RIGHTSHOW', {rightShow: 1})
             this.$refs.error.play()
+            this.$store.commit('learnWords/RIGHTSHOW', {rightShow: 1})
           }
         }
       }
